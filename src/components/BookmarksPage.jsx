9bd859e3e4f2ea82e7ba3ba6ca0044a845639d2e
amandk5 <adkofficial01@gmail.com>
2023-06-22 23:27:52 +0530

    fixed bug in bookmarkspage

--- src/components/BookmarksPage.jsx
+++ src/components/BookmarksPage.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 export default function BookmarksPage() {
   const [bookmarkedPokemon, setBookmarkedPokemon] = useState(
-    JSON.parse(localStorage.getItem("bookmarkedPokemon") || [])
+    JSON.parse(localStorage.getItem("bookmarkedPokemon")) || []
   );
 
   // set current hovered
